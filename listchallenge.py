a97abb587bda130e00d0b79e567b11105672ebb2
bmead92 <bryce.meadors@gmail.com>
2023-01-10 18:03:47 +0000

    list challenges and manipulation

---
+++ listchallenge.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+heroes= ["Spiderman", "Batman", "Black Panther", "Wonder Woman", "Storm"]
+
+# PART 1
+# Print out your favorite character from this list! The output would look something like:
+# My favorite character is Black Panther!
+print(f'My favorite character is {heroes[0]}!')
+
+# PART 2
+# Ask the user to pick a number between 1 and 100.
+# Convert the input into an integer.
+user_number = input("Please type a number between 1 and 100\n")
+user_number = int(user_number)
+print(f"You typed: {user_number}")
+
+nums= [1, -5, 56, 987, 0]
+
+# PART 3
+# check out https://docs.python.org/3/library/functions.html or go to Google
+# use a built-in function to find which integer in nums is the biggest.
+# then, print out that number!
+print(f"Largest int from nums list is : {max(nums)}")
--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
+import random
 my_list = ['192.168.0.5', 5060, 'UP' ]
 iplist = [5060, '80', 55, '10.0.0.1', '10.20.30.1']
 print('The first item in the list (IP): ' + my_list[0] )
 print("The second item in the list (port): " + str(my_list[1]) )
 print("The last item in the list (state): " + my_list[2] )
 print('IP address from iplist: ' + iplist[3] + ' and ' + iplist[4])
+wordbank= ["indentation", "spaces"] 
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+wordbank.append(4)
+num = input(f'Please type in a number between 1 and {len(tlgstudents)} \n')
+num = int(num)
+student_name = tlgstudents[num - 1]
+print(f"{random.choice(tlgstudents)} always uses {wordbank[-1]} {wordbank[1]} to indent.")
 
