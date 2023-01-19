8af54a86e3c650dd44aba43bcb05368edd8cfb2d
bmead92 <bryce.meadors@gmail.com>
2023-01-19 21:04:16 +0000

    sql operations and python3

---
+++ my_projects/trivia_game.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import requests
+
+URL = "https://opentdb.com/api.php?amount=3&category=9&difficulty=easy&type=boolean"
+
+def main():
+
+    # data will be a python dictionary rendered from your API link's JSON!
+    data= requests.get(URL).json()
+    # display the question
+    print(f"Question - {data['results'][0]['question']}")
+    # display the answers
+    print(f"Correct answer - {data['results'][0]['correct_answer']}")
+    print(f"Incorrect answer - {data['results'][0]['incorrect_answers']}")
+if __name__ == "__main__":
+    main()
+
